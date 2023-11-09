b999f2e39f8b7d6d840cd003d4470a0ba7c9a49d
thieryw <williamalexthiery@hotmail.fr>
2023-11-09 21:12:33 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.NODE_ENV === "production" ? "/test" : "";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base,
+  "define": {
+    "__BASE_URL__": JSON.stringify(base)
+  }
 })
+
+	
